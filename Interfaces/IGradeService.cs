7c243a122c1770338be3192774372b6256709134
Carla <126351324+carla-draghiciu@users.noreply.github.com>
2026-05-08 20:37:59 +0300

    created grade service

---
+++ Interfaces/IGradeService.cs
@@ -0,0 +1,10 @@
+﻿using Siemens.Internship2026.GradeBook.Models;
+
+namespace Siemens.Internship2026.GradeBook.Interfaces
+{
+    public interface IGradeService
+    {
+        Task<List<Grade>> GetAllGradesAsList();
+        Task<Grade?> GetGradeById(int searchedGradeId);
+    }
+}
---
+++ Services/GradeService.cs
@@ -0,0 +1,25 @@
+﻿using Siemens.Internship2026.GradeBook.Interfaces;
+using Siemens.Internship2026.GradeBook.Models;
+
+namespace Siemens.Internship2026.GradeBook.Services
+{
+    public class GradeService : IGradeService
+    {
+        private readonly IGradeRepository _gradeRepository;
+
+        public GradeService(IGradeRepository gradeRepository)
+        {
+            this._gradeRepository = gradeRepository;
+        }
+
+        public async Task<List<Grade>> GetAllGradesAsList()
+        {
+            return (await this._gradeRepository.GetAllGradesAsync()).ToList();
+        }
+
+        public async Task<Grade?> GetGradeById(int searchedGradeId)
+        {
+            return await this._gradeRepository.GetGradeByIdAsync(searchedGradeId);
+        }
+    }
+}
