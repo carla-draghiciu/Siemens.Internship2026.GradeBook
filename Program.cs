fac05d9d9afe181b62ab0670c30aa35e6f0f0eb2
Carla <126351324+carla-draghiciu@users.noreply.github.com>
2026-05-08 18:37:01 +0300

    added the repo to program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
 using Siemens.Internship2026.GradeBook.Interfaces;
+using Siemens.Internship2026.GradeBook.Repositories;
 using Siemens.Internship2026.GradeBook.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<IItemRepository, ItemRepository>();
+builder.Services.AddScoped<IItemReader>(sp => sp.GetRequiredService<IItemRepository>());
+builder.Services.AddScoped<IItemWriter>(sp => sp.GetRequiredService<IItemRepository>());
 builder.Services.AddScoped<IItemService, ItemService>();
 builder.Services.AddScoped<ILoggerService, ConsoleLoggerService>();
 
