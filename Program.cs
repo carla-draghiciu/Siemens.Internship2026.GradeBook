65670778056fbde8535caf8af1b0e136bb0f2ea7
Carla <126351324+carla-draghiciu@users.noreply.github.com>
2026-05-08 18:10:59 +0300

    added logger service in program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 using Siemens.Internship2026.GradeBook.Interfaces;
+using Siemens.Internship2026.GradeBook.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddScoped<IItemService, IItemService>();
+builder.Services.AddScoped<ILoggerService, ConsoleLoggerService>();
 
 var app = builder.Build();
 
