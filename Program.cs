2882f0b9eff932b645f8bb9434d6680d0090b984
Carla <126351324+carla-draghiciu@users.noreply.github.com>
2026-05-08 18:11:38 +0300

    fixed item service in program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using Siemens.Internship2026.GradeBook.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddScoped<IItemService, IItemService>();
+builder.Services.AddScoped<IItemService, ItemService>();
 builder.Services.AddScoped<ILoggerService, ConsoleLoggerService>();
 
 var app = builder.Build();
