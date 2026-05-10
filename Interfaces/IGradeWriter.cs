22e33b0f54ce9cd054033a6df3e84480a0cfaaa3
Carla <126351324+carla-draghiciu@users.noreply.github.com>
2026-05-10 12:56:04 +0300

    removed an unused import

--- Interfaces/IGradeWriter.cs
+++ Interfaces/IGradeWriter.cs
@@ -5,7 +5,7 @@ namespace Siemens.Internship2026.GradeBook.Interfaces
     public interface IGradeWriter
     {
         Task AddGradeAsync(Grade gradeToBeAdded);
-        Task<bool> DeleteGradeAsync(int idOfGradeToDelete);
         Task<bool> UpdateGradeAsync(int idOfGradeToUpdate, Grade newGrade);
+        Task<bool> DeleteGradeAsync(int idOfGradeToDelete);
     }
 }
--- Services/GradeStatisticsService.cs
+++ Services/GradeStatisticsService.cs
@@ -1,5 +1,4 @@
 ﻿using Siemens.Internship2026.GradeBook.Interfaces;
-using Siemens.Internship2026.GradeBook.Models;
 
 namespace Siemens.Internship2026.GradeBook.Services
 {
